9c9ecf8a664710f4747d57543712fd30f59b7b7b
MODIxPLAYS <parthivmodi10@gmail.com>
2025-09-29 15:29:08 -0400

    Lab2 Assignment - make index.css file

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -1,7 +1,18 @@
-export default function Page() {
+import "./index.css";
+export default function Lab2() {
   return (
-    <main style={{ padding: "20px", fontFamily: "Arial, sans-serif", lineHeight: "1.6" }}>
-      <h1>Lab 2 - Landing Page</h1>
-    </main>
+    <div id="wd-lab2">
+      <h2>Lab 2 - Cascading Style Sheets</h2>
+
+
+      
+      <h3>Styling with the STYLE attribute</h3>
+      <p style={{ backgroundColor: "blue", color: "white" }}>
+        Style attribute allows configuring look and feel
+        right on the element. Although it&apos;s very convenient
+        it is considered bad practice and you should avoid
+        using the style attribute
+      </p>
+    </div>
   );
 }
