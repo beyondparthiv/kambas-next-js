9d142aaf6fc01671acb8aa0a02cc586698e70e41
MODIxPLAYS <parthivmodi10@gmail.com>
2025-10-02 02:48:58 -0400

    lab2(bootstrap): add bottom-right breakpoint badge visible only on Lab 2

--- app/labs/lab2/layout.tsx
+++ app/labs/lab2/layout.tsx
@@ -1,13 +1,30 @@
+
 import { ReactNode } from "react";
 import "./index.css";
 
 export const metadata = { title: "Lab 2" };
 
 export default function Lab2Layout({ children }: { children: ReactNode }) {
-  // Wrap the whole page in a Bootstrap container with thin padding
   return (
-    <div id="wd-lab2-layout" className="container p-2">
-      {children}
-    </div>
+    <>
+      {/* Rubric: responsive breakpoint badge (bottom-right), Lab 2 only */}
+      <div
+        id="wd-breakpoint"
+        className="position-fixed bottom-0 end-0 m-2 px-2 py-1 bg-dark text-white rounded"
+        style={{ zIndex: 1050, fontSize: 12 }}
+      >
+        <span className="d-inline d-sm-none">xs</span>
+        <span className="d-none d-sm-inline d-md-none">sm</span>
+        <span className="d-none d-md-inline d-lg-none">md</span>
+        <span className="d-none d-lg-inline d-xl-none">lg</span>
+        <span className="d-none d-xl-inline d-xxl-none">xl</span>
+        <span className="d-none d-xxl-inline">xxl</span>
+      </div>
+
+      {/* Lab 2 content wrapper */}
+      <div id="wd-lab2-layout" className="container p-2">
+        {children}
+      </div>
+    </>
   );
 }
