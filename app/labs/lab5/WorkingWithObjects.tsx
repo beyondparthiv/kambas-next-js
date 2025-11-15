db7324aa76bf54f6467207a87d1288b6971a2e0a
beyondparthiv <parthivmodi10@gamil.com>
2025-11-15 17:42:56 -0500

    All labs rubric done

--- app/labs/lab5/WorkingWithObjects.tsx
+++ app/labs/lab5/WorkingWithObjects.tsx
@@ -1,66 +1,93 @@
 "use client";
-import React from "react";
-
+import React, { useState } from "react";
+import { FormControl } from "react-bootstrap";
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
-
 export default function WorkingWithObjects() {
+  const [assignment, setAssignment] = useState({
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10",
+    completed: false,
+    score: 0,
+  });
+  const [module, setModule] = useState({
+    id: 2,
+    name: "Module",
+    description: "Example of a module",
+    course: "CS4550"
+  })
+  const ASSIGNMENT_API_URL = `${HTTP_SERVER}/lab5/assignment`;
+  const MODULE_API_URL = `${HTTP_SERVER}/lab5/module`;
   return (
     <div id="wd-working-with-objects">
-      <h3>Working with Objects</h3>
-
-      {/* Get Assignment */}
-      <h4>Get Assignment</h4>
+      <h3>Working With Objects</h3>
+      <h4>Modifying Properties</h4>
+      <a id="wd-update-assignment-title"
+         className="btn btn-primary float-end"
+         href={`${ASSIGNMENT_API_URL}/title/${assignment.title}`}>
+        Update Title </a>
+      <FormControl className="w-75" id="wd-assignment-title"
+        defaultValue={assignment.title} onChange={(e) =>
+          setAssignment({ ...assignment, title: e.target.value })}/>
+      <hr />
+      <a id="wd-update-assignment-score"
+         className="btn btn-primary float-end"
+         href={`${ASSIGNMENT_API_URL}/score/${assignment.score}`}>
+        Update Score </a>
+      <FormControl className="w-75" id="wd-assignment-score"
+        defaultValue={assignment.score} onChange={(e) =>
+          setAssignment({ ...assignment, score: parseInt(e.target.value) })}/>
+      <hr />
+      <a id="wd-update-assignment-completed"
+         className="btn btn-primary float-end"
+         href={`${ASSIGNMENT_API_URL}/completed/${assignment.completed}`}>
+        Update Completed Status </a>
+      <FormControl className="w-75" id="wd-assignment-completed"
+        defaultValue={assignment.completed == true ? "true" : "false"} onChange={(e) =>
+          setAssignment({ ...assignment, completed: e.target.value == "true" ? true : false})}/>
+      <hr />
+      <a id="wd-update-module-name"
+         className="btn btn-primary float-end"
+         href={`${MODULE_API_URL}/name/${module.name}`}>
+        Update Module Name </a>
+      <FormControl className="w-75" id="wd-module-name"
+        defaultValue={module.name} onChange={(e) =>
+          setModule({ ...module, name: e.target.value })}/>
+      <hr />
+      <h4>Retrieving Objects</h4>
       <a
-        id="wd-retrieve-assignment"
+        id="wd-retrieve-assignments"
+        className="btn btn-primary"
         href={`${HTTP_SERVER}/lab5/assignment`}
-        className="btn btn-primary mb-2"
       >
         Get Assignment
       </a>
-      <hr />
-
-      {/* Get Title */}
-      <h4>Get Title</h4>
       <a
-        id="wd-retrieve-assignment-title"
-        href={`${HTTP_SERVER}/lab5/assignment/title`}
-        className="btn btn-primary mb-2"
-      >
-        Get Title
-      </a>
-      <hr />
-
-      {/* Update Title */}
-      <h4>Update Title</h4>
-      <a
-        id="wd-update-title-nodejs"
-        href={`${HTTP_SERVER}/lab5/assignment/title/UpdatedTitle`}
-        className="btn btn-warning mb-2"
-      >
-        Update Title
-      </a>
-      <hr />
-
-      {/* Get Module */}
-      <h4>Get Module</h4>
-      <a
-        id="wd-retrieve-module"
+        id="wd-retrieve-assignments"
+        className="btn btn-primary"
         href={`${HTTP_SERVER}/lab5/module`}
-        className="btn btn-primary mb-2"
       >
         Get Module
       </a>
       <hr />
-
-      {/* Get Module Name */}
-      <h4>Get Module Name</h4>
+      <h4>Retrieving Properties</h4>
+      <a
+        id="wd-retrieve-assignment-title"
+        className="btn btn-primary"
+        href={`${HTTP_SERVER}/lab5/assignment/title`}
+      >
+        Get Title
+      </a>
+      <hr />
       <a
         id="wd-retrieve-module-name"
+        className="btn btn-primary"
         href={`${HTTP_SERVER}/lab5/module/name`}
-        className="btn btn-primary mb-2"
       >
-        Get Module Name
+        Get Name
       </a>
+      <hr />
     </div>
   );
 }
